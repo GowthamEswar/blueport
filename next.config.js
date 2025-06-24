58d09f628eef638b0fb03fc48f48c420047a70d8
GowthamEswar <gowthameswar8@gmail.com>
2025-06-24 18:54:40 +0530

    Update application: 6/24/2025, 6:54:00 PM

--- next.config.js
+++ next.config.js
@@ -4,12 +4,20 @@ const nextConfig = {
     appDir: true
   },
   images: {
-    domains: [
-      "images.unsplash.com",
-      "randomuser.me"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '/api/portraits/**'
+      }
     ]
   },
-  output: "standalone",
-  runtime: "edge"
+  output: 'standalone',
+  runtime: 'edge'
 };
 module.exports = nextConfig;
